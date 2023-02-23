2f226616a0110902b2ea6dfa2e5670ace71de3b3
Nacho <amigoigna@gmail.com>
2023-02-23 17:41:12 -0300

    Correctly create curve

--- crypto/src/hash/pedersen/mod.rs
+++ crypto/src/hash/pedersen/mod.rs
@@ -3,9 +3,8 @@ use lambdaworks_math::{
     elliptic_curve::{
         short_weierstrass::{
             curves::bls12_381::{curve::BLS12381Curve, field_extension::BLS12381PrimeField},
-            element::ProjectivePoint,
         },
-        traits::IsEllipticCurve,
+        traits::IsEllipticCurve//, point::ProjectivePoint,
     },
     field::{self, element::FieldElement, traits::IsField},
     traits::ByteConversion,
@@ -13,10 +12,10 @@ use lambdaworks_math::{
 };
 
 type FE = field::element::FieldElement<BLS12381PrimeField>;
-type Point = ProjectivePoint<BLS12381Curve>;
+//type Point = ProjectivePoint<BLS12381Curve>;
 
 const WINDOW_SIZE: usize = 4;
-const NUM_WINDOWS: usize = 96;
+const NUM_WINDOWS: usize = 128;
 const INPUT_SIZE_IN_BITS: usize = WINDOW_SIZE * NUM_WINDOWS;
 const HALF_INPUT_SIZE_BITS: usize = INPUT_SIZE_IN_BITS / 2;
 
@@ -122,112 +121,55 @@ mod tests {
     use super::*;
     use crate::hash::{pedersen::Pedersen, traits::IsCryptoHash};
     use lambdaworks_math::{
-        elliptic_curve::short_weierstrass::traits::IsShortWeierstrass,
+        elliptic_curve::{
+            traits::IsEllipticCurve, short_weierstrass::{traits::IsShortWeierstrass, point::ShortWeierstrassProjectivePoint, curves::bls12_377::curve::BLS12377Curve},
+        },
         field::{
             element::FieldElement,
             fields::u384_prime_field::{IsMontgomeryConfiguration, MontgomeryBackendPrimeField},
         },
-        unsigned_integer::element::{U256, U384},
+        unsigned_integer::element::{U384},
     };
 
-    // Define starkware-rs field and field element.
     #[derive(Clone, Debug)]
     pub struct TestFieldConfig;
     impl IsMontgomeryConfiguration for TestFieldConfig {
         const MODULUS: U384 =
             U384::from("800000000000011000000000000000000000000000000000000000000000001");
-        const MP: u64 = 18446744073709551615u64;
-        const R2: U384 =
-            U384::from("38E5F79873C0A6DF47D84F8363000187545706677FFCC06CC7177D1406DF18E");
+        const MP: u64 = 18446744073709551615;
+        const R2: U384 = U384::from("38e5f79873c0a6df47d84f8363000187545706677ffcc06cc7177d1406df18e");
     }
-
+    
     type PedersenTestField = MontgomeryBackendPrimeField<TestFieldConfig>;
     type TestFieldElement = FieldElement<PedersenTestField>;
-
+    
     // Define starkware-rs elliptic curve.
     #[derive(Clone, Debug)]
     pub struct TestCurve;
-
+    
     impl IsShortWeierstrass for TestCurve {
-        type UIntOrders = U384;
-
         fn a() -> FieldElement<Self::BaseField> {
-            let alpha = Self::UIntOrders {
-                limbs: [
-                    0,
-                    0,
-                    18446744073709551585,
-                    18446744073709551615,
-                    18446744073709551615,
-                    576460752303422960,
-                ],
-            };
-            FieldElement::<Self::BaseField>::from(&alpha)
+            let alpha = U384::from_u64(1);
+            FieldElement::<Self::BaseField>::new(alpha)
         }
-
+    
         fn b() -> FieldElement<Self::BaseField> {
-            let beta = Self::UIntOrders {
-                limbs: [
-                    0,
-                    0,
-                    3863487492851900874,
-                    7432612994240712710,
-                    12360725113329547591,
-                    88155977965380735,
-                ],
-            };
-            FieldElement::<Self::BaseField>::from(&beta)
-        }
-
-        fn order_r() -> Self::UIntOrders {
-            todo!()
-        }
-
-        fn order_p() -> Self::UIntOrders {
-            todo!()
-        }
-
-        fn target_normalization_power() -> Vec<u64> {
-            todo!()
+            let beta = U384::from("6f21413efbe40de150e596d72f7a8c5609ad26c15c915c1f4cdfcb99cee9e89");
+            FieldElement::<Self::BaseField>::new(beta)
         }
     }
-
+    
     impl IsEllipticCurve for TestCurve {
         type BaseField = PedersenTestField;
-        type PointRepresentation = TestCurveProjectivePoint;
-
-        fn generator() -> Self::PointRepresentation {
-            let x = FieldElement::<Self::BaseField>::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    14484022957141291997,
-                    5884444832209845738,
-                    299981207024966779,
-                    232005955912912577,
-                ],
-            });
-            let y = FieldElement::<Self::BaseField>::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    6241159653446987914,
-                    664812301889158119,
-                    18147424675297964973,
-                    405578048423154473,
-                ],
-            });
-            Self::create_point_from_affine(x, y)
-        }
-
-        fn create_point_from_affine(
-            x: FieldElement<Self::BaseField>,
-            y: FieldElement<Self::BaseField>,
-        ) -> Self::PointRepresentation {
-            Self::PointRepresentation::new([x, y, FieldElement::one()])
+        type PointRepresentation = ShortWeierstrassProjectivePoint<TestCurve>;
+    
+        fn generator() -> ShortWeierstrassProjectivePoint<TestCurve> {
+            let x = FieldElement::<Self::BaseField>::new(U384::from("1ef15c18599971b7beced415a40f0c7deacfd9b0d1819e03d723d8bc943cfca"));
+            let y = FieldElement::<Self::BaseField>::new(U384::from("5668060aa49730b7be4801df46ec62de53ecd11abe43a32873000c36e8dc1f"));
+            Self::create_point_from_affine(x, y).unwrap()
         }
     }
-
+    
     fn to_bits(felt: TestFieldElement) -> Vec<bool> {
         let felt_bytes = felt.value().to_bytes_be();
         let mut bits = Vec::with_capacity(felt_bytes.len() * 8);
@@ -245,7 +187,6 @@ mod tests {
                 parameters: Self::create_generators(&mut rand::thread_rng()),
             }
         }
-
         fn hash_one(&self, input: TestFieldElement) -> TestFieldElement {
             // Compute sum of h_i^{m_i} for all i.
             let bits = to_bits(input);
@@ -264,28 +205,24 @@ mod tests {
                 // not implemented for FieldElement yet.
                 .fold(TestFieldElement::zero(), |acc, x| acc + x)
         }
-
+    
         fn hash_two(&self, left: TestFieldElement, right: TestFieldElement) -> TestFieldElement {
-            let left_input_bytes = left.value().to_bytes_be()[48 - 32..].to_vec();
-            let right_input_bytes = right.value().to_bytes_be()[48 - 32..].to_vec();
-            let mut buffer = vec![0u8; (256) / 8];
-
+            let left_input_bytes = left.value().to_bytes_be();
+            let right_input_bytes = right.value().to_bytes_be();
+            let mut buffer = vec![0u8; (HALF_INPUT_SIZE_BITS + HALF_INPUT_SIZE_BITS) / 8];
+    
             buffer
                 .iter_mut()
                 .zip(left_input_bytes.iter().chain(right_input_bytes.iter()))
                 .for_each(|(b, l_b)| *b = *l_b);
-            for _ in 0..16 {
-                buffer.insert(0, 0);
-            }
+    
             let base_type_value = U384::from_bytes_be(&buffer).unwrap();
             let new_input_value = PedersenTestField::from_base_type(base_type_value);
             let new_input = TestFieldElement::from(&new_input_value);
-
+    
             self.hash_one(new_input)
         }
     }
-
-    type TestCurveProjectivePoint = ProjectivePoint<TestCurve>;
 
     #[test]
     fn test_pedersen_hash() {
@@ -299,102 +236,44 @@ mod tests {
             "030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662",
         ));
 
-        let pederse_p0 = TestCurve::create_point_from_affine(
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    3602345268353203007,
-                    13758484295849329960,
-                    518715844721862878,
-                    241691544791834578,
-                ]
-            }),
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    13441546676070136227,
-                    13001553326386915570,
-                    433857700841878496,
-                    368891789801938570,
-                ]
-            }),
-        );
+        let _pederse_p0 = TestCurve::create_point_from_affine(
+            TestFieldElement::new(U384::from("49EE3EBA8C1600700EE1B87EB599F16716B0B1022947733551FDE4050CA6804")),
+            TestFieldElement::new(U384::from("3CA0CFE4B3BC6DDF346D49D06EA0ED34E621062C0E056C1D0405D266E10268A"))
+        ).unwrap();
 
         let pederse_p1 = TestCurve::create_point_from_affine(
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    16491878934996302286,
-                    12382025591154462459,
-                    10043949394709899044,
-                    253000153565733272,
-                ]
-            }),
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    13950428914333633429,
-                    2545498000137298346,
-                    5191292837124484988,
-                    285630633187035523,
-                ]
-            }),
-        );
+            TestFieldElement::new(U384::from("234287DCBAFFE7F969C748655FCA9E58FA8120B6D56EB0C1080D17957EBE47B")),
+            TestFieldElement::new(U384::from("3B056F100F96FB21E889527D41F4E39940135DD7A6C94CC6ED0268EE89E5615")),
+        ).unwrap();
 
         let pederse_p2 = TestCurve::create_point_from_affine(
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    1203723169299412240,
-                    18195981508842736832,
-                    12916675983929588442,
-                    338510149841406402,
-                ]
-            }),
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    12352616181161700245,
-                    11743524503750604092,
-                    11088962269971685343,
-                    161068411212710156,
-                ]
-            }),
-        );
+            TestFieldElement::new(U384::from("4FA56F376C83DB33F9DAB2656558F3399099EC1DE5E3018B7A6932DBA8AA378")),
+            TestFieldElement::new(U384::from("3FA0984C931C9E38113E0C0E47E4401562761F92A7A23B45168F4E80FF5B54D")),
+        ).unwrap();
 
         let pederse_p3 = TestCurve::create_point_from_affine(
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    1145636535101238356,
-                    10664803185694787051,
-                    299781701614706065,
-                    425493972656615276,
-                ]
-            }),
-            TestFieldElement::from(&U384 {
-                limbs: [
-                    0,
-                    0,
-                    8187986478389849302,
-                    4428713245976508844,
-                    6033691581221864148,
-                    345457391846365716,
-                ]
-            }),
-        );
+            TestFieldElement::new(U384::from("4BA4CC166BE8DEC764910F75B45F74B40C690C74709E90F3AA372F0BD2D6997")),
+            TestFieldElement::new(U384::from("40301CF5C1751F4B971E46C4EDE85FCAC5C59A5CE5AE7C48151F27B24B219C")),
+        ).unwrap();
 
-        let parameters = vec![pederse_p0, pederse_p1, pederse_p2, pederse_p3];
+        let pederse_p4 = TestCurve::create_point_from_affine(
+            TestFieldElement::new(U384::from("54302DCB0E6CC1C6E44CCA8F61A63BB2CA65048D53FB325D36FF12C49A58202")),
+            TestFieldElement::new(U384::from("1B77B3E37D13504B348046268D8AE25CE98AD783C25561A879DCC77E99C2426")),
+        ).unwrap();
 
-        // let hasher: Pedersen<TestCurve> = Pedersen::from_parameters(parameters);
 
-        // assert_eq!(hasher.hash_two(in1, in2), expected_hash);
+        let in1 = FE::new_base("03d937c035c878245caf64531a5756109c53068da139362728feb561405371cb");
+        let in2 = FE::new_base("0208a0a10250e382e1e4bbe2880906c2791bf6275695e02fbbc6aeff9cd8b31a");
+        let expected_hash = FE::new_base("030e480bed5fe53fa909cc0f8c4d99b8f9f2c016be4c41e13a4848797979c662");
+
+        let hasher = Pedersen::new();
+
+        assert_eq!(hasher.hash_two(in1, in2), expected_hash);
+
+        let parameters = vec![ pederse_p1, pederse_p2, pederse_p3, pederse_p4];
+        let parameters = parameters.into_iter().map(|x| x.coordinates().to_vec()).collect();
+        let hasher: Pedersen<TestCurve> = Pedersen::from_parameters(parameters);
+
+       // assert_eq!(hasher.hash_two(in1, in2), expected_hash);
     }
 }
