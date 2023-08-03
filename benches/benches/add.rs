b7fb776983e4f83215bf7af51c63267e23cba384
Sergio Chouhy <sergio.chouhy@gmail.com>
2023-08-02 21:13:33 -0300

    make add and mul benches cumulative

--- benches/benches/add.rs
+++ benches/benches/add.rs
@@ -10,21 +10,24 @@ pub mod utils;
 const BENCHMARK_NAME: &str = "add";
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let arkworks_vec = generate_random_elements(2000000);
+    let arkworks_vec = generate_random_elements(10_000_000);
 
     // arkworks-ff
     {
         c.bench_function(
-            &format!("{} 1M elements | ark-ff - ef8f758", BENCHMARK_NAME),
+            &format!("{} 10M elements | ark-ff - ef8f758", BENCHMARK_NAME),
             |b| {
                 b.iter(|| {
                     let mut iter = arkworks_vec.iter();
+                    let a = iter.next().unwrap();
+                    let b = iter.next().unwrap();
+                    let mut c = a.add(b);
 
-                    for _i in 0..1000000 {
+                    for _i in 2..10_000_000 {
                         let a = iter.next().unwrap();
-                        let b = iter.next().unwrap();
-                        black_box(black_box(&a).add(black_box(b)));
+                        c = c.add(a);
                     }
+                    black_box(c);
                 });
             },
         );
@@ -35,16 +38,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let lambdaworks_vec = to_lambdaworks_vec(&arkworks_vec);
 
         c.bench_function(
-            &format!("{} 1M elements | lambdaworks", BENCHMARK_NAME,),
+            &format!("{} 10M elements | lambdaworks", BENCHMARK_NAME,),
             |b| {
                 b.iter(|| {
                     let mut iter = lambdaworks_vec.iter();
+                    let a = iter.next().unwrap();
+                    let b = iter.next().unwrap();
+                    let mut c = a.add(b);
 
-                    for _i in 0..1000000 {
+                    for _i in 2..10_000_000 {
                         let a = iter.next().unwrap();
-                        let b = iter.next().unwrap();
-                        black_box(black_box(&a).add(black_box(b)));
+                        c =c.add(a);
                     }
+                    black_box(c);
                 });
             },
         );
--- benches/benches/mul.rs
+++ benches/benches/mul.rs
@@ -9,24 +9,27 @@ pub mod utils;
 const BENCHMARK_NAME: &str = "mul";
 
 pub fn criterion_benchmark(c: &mut Criterion) {
-    let arkworks_vec = generate_random_elements(20000);
+    let arkworks_vec = generate_random_elements(2_000_000);
 
     // arkworks-ff
     {
         c.bench_function(
             &format!(
-                "{} 10K elements | ark-ff - commit: ef8f758 ",
+                "{} 2M elements | ark-ff - commit: ef8f758 ",
                 BENCHMARK_NAME
             ),
             |b| {
                 b.iter(|| {
                     let mut iter = arkworks_vec.iter();
+                    let a = iter.next().unwrap();
+                    let b = iter.next().unwrap();
+                    let mut c = a.mul(b);
 
-                    for _i in 0..10000 {
+                    for _i in 2..2_000_000 {
                         let a = iter.next().unwrap();
-                        let b = iter.next().unwrap();
-                        black_box(black_box(a).mul(black_box(b)));
+                        c = c.mul(a);
                     }
+                    black_box(c);
                 });
             },
         );
@@ -37,16 +40,19 @@ pub fn criterion_benchmark(c: &mut Criterion) {
         let lambdaworks_vec = to_lambdaworks_vec(&arkworks_vec);
 
         c.bench_function(
-            &format!("{} 10K elements | lambdaworks", BENCHMARK_NAME,),
+            &format!("{} 2M elements | lambdaworks", BENCHMARK_NAME,),
             |b| {
                 b.iter(|| {
                     let mut iter = lambdaworks_vec.iter();
+                    let a = iter.next().unwrap();
+                    let b = iter.next().unwrap();
+                    let mut c = a.mul(b);
 
-                    for _i in 0..10000 {
+                    for _i in 2..2_000_000 {
                         let a = iter.next().unwrap();
-                        let b = iter.next().unwrap();
-                        black_box(black_box(&a).mul(black_box(b)));
+                        c =c.mul(a);
                     }
+                    black_box(c);
                 });
             },
         );
