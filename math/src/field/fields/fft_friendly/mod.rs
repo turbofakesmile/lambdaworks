d503c3d1677a04dcd33de002592ceecae6beb203
PatStiles <pdstiles78@gmail.com>
2023-09-22 00:21:04 -0500

    add bench for U64 mont and mersenne31

--- math/src/field/fields/fft_friendly/mod.rs
+++ math/src/field/fields/fft_friendly/mod.rs
@@ -2,3 +2,5 @@
 pub mod babybear;
 /// Implementation of two-adic prime field over 256 bit unsigned integers.
 pub mod stark_252_prime_field;
+/// Implemenation of the Mersenne Prime field p = 2^31 - 1
+pub mod u64_mersenne_montgomery_field;
