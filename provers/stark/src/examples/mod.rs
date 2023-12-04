5e06ce872338d50605464eedb1eb0ddcc656acb3
Mariano Nicolini <mariano.nicolini.91@gmail.com>
2023-12-04 11:18:21 -0300

    Add example module

--- provers/stark/src/examples/mod.rs
+++ provers/stark/src/examples/mod.rs
@@ -2,6 +2,7 @@ pub mod dummy_air;
 pub mod fibonacci_2_cols_shifted;
 pub mod fibonacci_2_columns;
 pub mod fibonacci_rap;
+pub mod multi_step_air;
 pub mod quadratic_air;
 pub mod simple_fibonacci;
 pub mod simple_periodic_cols;
